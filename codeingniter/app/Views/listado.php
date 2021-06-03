c568819cf24d65347158f641f899ec2a2104aef4
SantiagoBtc6 <84112242+SantiagoBtc6@users.noreply.github.com>
2021-06-03 10:03:08 -0500

    Update listado.php

--- codeingniter/app/Views/listado.php
+++ codeingniter/app/Views/listado.php
@@ -8,14 +8,13 @@
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 
-    <title>santiago!</title>
+    <title>Alan!</title>
   </head>
   <body>
 <body>
 <div class="container">
 <h1>Agregar Registro</h1>
-<div
-class="row">
+<div class="row">
 <div class="col-sm-12">
 <form action="">
 <label for="nombre">Nombre:</label>
@@ -28,12 +27,28 @@ class="row">
 </form>
 </div>
 </div>
+<div class="row">
+<div class="col-sm-12">
+<div class="table table-resonsive">
+<table class="table table-hover table-bordered">
+<tr>
+<th>Nombre</th>
+<th>Apellido paterno</th>
+<th>Apellido materno</th>
+<th>Editar</th>
+<th>Eliminar</th>
+</tr>
+</table>
+</div>
+</div>
+</div>
+
 </div>
 
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
   </body>
 </html>
